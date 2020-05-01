4e1c008037c140aba93ae537343f9c83a7170cbd
Abinaya Ramachandran <abinayaksrv@gmail.com>
2020-04-30 17:20:11 -0700

    Pushing Node Express Project to git

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
-// const express = require('express');
-const app = express();
 const vision = require('@google-cloud/vision');
 // Creates a client
 const client = new vision.ImageAnnotatorClient({
   keyFilename: 'views/samplevisionkey.json'   // here you can use your key generated
 });
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // console.log('index log - ' , req)
@@ -16,27 +13,23 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/send', function(req, res, next) {
-  console.log('index log - -------------------------------', req.body )
     client
-        .labelDetection('views/lady.jpg')
+        .labelDetection('views/lady.jpg')// here we need to replace the image with image from request
         .then(results => {
             const labels = results[0].labelAnnotations;
-
             console.log('Labels:');
             labels.forEach(label => console.log(label));
-            //console.log(results);
             console.log(labels[0] , labels[0].score)
+            //We need to manipulate the json n send it a format need in the front end(index.ejs)
             res.render('index', { data: labels[0].score , title: 'Express'});
         })
         .catch(err => {
             console.error('ERROR:', err);
             res.render('index', { data: req.body , title: 'Express'});
         });
-  // res.render('index', { data: req.body , title: 'Express'});
+
 });
 
-// app.post('/example', (req, res) => {
-//   res.send(`Full name is:${req.body.fname} ${req.body.lname}.`);
-// });
+
 
 module.exports = router;

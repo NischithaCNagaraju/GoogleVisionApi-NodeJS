c3e7df6fb508f4b5252eed2b98f0b338b3110697
NischithaCNagaraju <61173798+NischithaCNagaraju@users.noreply.github.com>
2020-05-01 20:26:34 -0700

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,56 @@
 var express = require('express');
 var router = express.Router();
+// const express = require('express');
+const multer = require("multer");
+var upload = multer({ dest: 'uploads/' });
+const app = express();
 const vision = require('@google-cloud/vision');
 // Creates a client
 const client = new vision.ImageAnnotatorClient({
-  keyFilename: 'views/samplevisionkey.json'   // here you can use your key generated
+    keyFilename: 'views/samplevisionkey.json'   // here you can use your key generated
 });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // console.log('index log - ' , req)
+    // console.log('index log - ' , req)
 
-  res.render('index');
+    res.render('index');
 });
 
-router.post('/send', function(req, res, next) {
+/*router.post('/send', function(req, res, next) {
+    console.log('index log - -------------------------------', req.body )
+   // console.log(req.file.pic);
+    var keyName=req.body.pic;
+    console.log(keyName);
     client
-        .labelDetection('views/lady.jpg')// here we need to replace the image with image from request
+        .labelDetection(keyName)
+        .then(results => {
+            const labels = results[0].labelAnnotations;
+
+            console.log('Labels:');
+            labels.forEach(label => console.log(label));
+            //console.log(results);
+            console.log(labels[0] , labels[0].score)
+            res.render('index', { data: labels[0].score , title: 'Express'});
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+            res.render('index', { data: req.body , title: 'Express'});
+        });
+    // res.render('index', { data: req.body , title: 'Express'});
+});*/
+
+router.post('/send', upload.single('pic'),  function(req, res, next) {
+    client
+        .labelDetection(req.file.path)// here we need to replace the image with image from request
         .then(results => {
             const labels = results[0].labelAnnotations;
             console.log('Labels:');
             labels.forEach(label => console.log(label));
             console.log(labels[0] , labels[0].score)
             //We need to manipulate the json n send it a format need in the front end(index.ejs)
-            res.render('index', { data: labels[0].score , title: 'Express'});
+
+            res.render('index', { data: labels[0].description , title: 'Express'});
         })
         .catch(err => {
             console.error('ERROR:', err);
@@ -29,7 +58,8 @@ router.post('/send', function(req, res, next) {
         });
 
 });
-
-
+// app.post('/example', (req, res) => {
+//   res.send(`Full name is:${req.body.fname} ${req.body.lname}.`);
+// });
 
 module.exports = router;

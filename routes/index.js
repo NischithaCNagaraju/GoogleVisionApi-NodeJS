e89cfa5cd989e4e5d7284c6277c12af7ab7aea21
NischithaCNagaraju <61173798+NischithaCNagaraju@users.noreply.github.com>
2020-05-05 11:57:19 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,17 @@
 var express = require('express');
 var router = express.Router();
-// const express = require('express');
-const multer = require("multer");
-var upload = multer({ dest: 'uploads/' });
-const app = express();
 const vision = require('@google-cloud/vision');
+const multer = require("multer")
+var upload = multer({ dest: 'uploads/' })
 // Creates a client
 const client = new vision.ImageAnnotatorClient({
     keyFilename: 'views/samplevisionkey.json'   // here you can use your key generated
 });
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     // console.log('index log - ' , req)
-
     res.render('index');
 });
-
 
 router.post('/send', upload.single('pic'),  function(req, res, next) {
     client
@@ -24,13 +19,17 @@ router.post('/send', upload.single('pic'),  function(req, res, next) {
         .then(results => {
             const labels = results[0].labelAnnotations;
             console.log('Labels:');
-            labels.forEach(label => console.log(label));
-            console.log(labels[0] , labels[0].score);
-
-            var arr = JSON.stringify(labels,  null, "    ");  // arr variable is sending the response to data
+            // labels.forEach(label => console.log(label));
+            result = []
+            for( label in labels){
+                console.log('labbb -- ' , labels[label]['description'], labels[label]['score'] )
+                result_data = {}
+                result_data['description'] = labels[label]['description']
+                result_data['score'] = labels[label]['score']
+                result.push(result_data)
+            }
             //We need to manipulate the json n send it a format need in the front end(index.ejs)
-            res.render('index', { data: arr, title: 'Express'});
-           // res.render('index', { data: labels[0].description , title: 'Express'});
+            res.render('index', { data: JSON.stringify(result) , title: 'Express'});
         })
         .catch(err => {
             console.error('ERROR:', err);
@@ -38,8 +37,7 @@ router.post('/send', upload.single('pic'),  function(req, res, next) {
         });
 
 });
-// app.post('/example', (req, res) => {
-//   res.send(`Full name is:${req.body.fname} ${req.body.lname}.`);
-// });
+
+
 
 module.exports = router;
